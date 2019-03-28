12e0a4cd6b222b7b530cc9e91dc87428ab061a48
zombaki <piyush.nath.2272@gmail.com>
2019-03-28 11:26:36 -0500

    1 file modified

--- app/hello.component.ts
+++ app/hello.component.ts
@@ -29,4 +29,14 @@ componenet_todo = [
     'Check e-mail',
     'Walk dog'
   ];
+   drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+    }
+  }
 }
